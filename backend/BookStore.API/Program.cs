ea62052be200cbe913ff2087e1cef1ae4a329c0e
jnewell-7 <jacksonnewell07@gmail.com>
2025-04-14 11:45:00 -0600

    Hilton commit

--- backend/BookStore.API/Program.cs
+++ backend/BookStore.API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "https://nice-rock-064f3f110.6.azurestaticapps.net")
+        policy.WithOrigins("http://localhost:3000", "https://jolly-island-020e61710.6.azurestaticapps.net")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials(); // Allow credentials
